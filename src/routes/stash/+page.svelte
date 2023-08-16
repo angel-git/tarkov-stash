3317cd983492a14f3cb3e3d7b75bbe2b147ba906
agavalda <agavalda@digital.ai>
2023-08-16 22:32:08 +0200

    #4 add loader

--- src/routes/stash/+page.svelte
+++ src/routes/stash/+page.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
 	import type { Item, Option, Profile } from '../../types';
-	import { profile } from '../../store';
+	import { profile, loading } from '../../store';
 	import StashGrid from './stash-grid.svelte';
 	import AmountModal from './modal/modal-amount.svelte';
-	import { invoke } from '@tauri-apps/api';
 	import { afterNavigate, goto } from '$app/navigation';
+	import Loading from '$lib/images/loading.gif';
+	import { invokeWithLoader } from '../../helper';
 
+	$: isLoading = $loading;
 	let selectedOption: Option | undefined;
 	let selectedItem: Item | undefined;
 
 	afterNavigate(() => {
-		invoke<Profile>('load_profile_file', {})
+		invokeWithLoader<Profile>('load_profile_file', {})
 			.then((p) => {
 				profile.set(p);
 			})
@@ -21,7 +23,7 @@
 
 	function handleOptionClicked(option: Option, item: Item) {
 		if (option === 'fir') {
-			invoke('change_fir', { item }).catch((e) => goto(`/error?message=${e}`));
+			invokeWithLoader('change_fir', { item }).catch((e) => goto(`/error?message=${e}`));
 			handleCloseModal();
 		} else {
 			selectedOption = option;
@@ -43,11 +45,23 @@
 		<h4>
 			Your current stash size is {$profile.sizeX}x{$profile.sizeY}
 		</h4>
+		{#if isLoading}
+			<img class="loading" src={Loading} alt="loading gif" width="40" height="40" />
+		{/if}
 		{#if selectedItem && selectedOption && selectedOption === 'amount'}
 			<AmountModal item={selectedItem} onClose={handleCloseModal} />
 		{/if}
 		<StashGrid profile={$profile} onOptionClicked={handleOptionClicked} />
 	{:else}
-		<p>loading profile</p>
+		<p>loading profile....</p>
 	{/if}
 </div>
+
+<style>
+	.loading {
+		position: fixed;
+		top: 12px;
+		right: 12px;
+		z-index: 10;
+	}
+</style>
