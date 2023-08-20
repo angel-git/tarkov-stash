5a744d9cf19259fa542021712ec17e86c4825ca4
agavalda <agavalda@digital.ai>
2023-08-20 14:14:17 +0200

    #29 change background color when hovering

--- src/routes/stash/stash-grid.svelte
+++ src/routes/stash/stash-grid.svelte
@@ -86,19 +86,19 @@
 			case 'black':
 				return `rgba(0, 0, 0, 0.3)`;
 			case 'blue':
-				return `rgba(63,63,147,0.1)`;
+				return `rgba(63, 63, 147, 0.1)`;
 			case 'green':
-				return `rgba(55,119,55,0.1)`;
+				return `rgba(55, 119, 55, 0.1)`;
 			case 'grey':
 				return `rgba(50, 50, 50, 0.1)`;
 			case 'orange':
-				return `rgba(255,182,115, 0.1)`;
+				return `rgba(255, 182, 115, 0.1)`;
 			case 'red':
 				return `rgba(255, 0, 0, 0.1)`;
 			case 'violet':
-				return `rgba(155,38,182, 0.1)`;
+				return `rgba(155, 38, 182, 0.1)`;
 			case 'yellow':
-				return `rgba(246,235,97, 0.1)`;
+				return `rgba(246, 235, 97, 0.1)`;
 			default:
 				return `rgba(0, 0, 0, 0.0)`;
 		}
@@ -187,7 +187,7 @@
 	}
 
 	.item-clickable:hover {
-		filter: brightness(120%);
+		background-color: rgba(44, 42, 42, 0.7) !important;
 	}
 
 	.fir {
