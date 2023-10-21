f00d79ad2f67a59bbe0c64c7fdebd42e3d8f3cd5
agavalda <agavalda@digital.ai>
2023-10-22 00:12:29 +0200

    #69 Add locales menu item

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -34,16 +34,51 @@ struct MutexState {
     pub bsg_items: Option<HashMap<String, Value>>,
     pub globals: Option<HashMap<String, Value>>,
     pub locale: Option<HashMap<String, Value>>,
+    pub locale_lang: String,
 }
 
 fn main() {
     let open = CustomMenuItem::new("open".to_string(), "Open profile");
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let submenu = Submenu::new("File", Menu::new().add_item(open).add_item(quit));
+    let locale_cz = CustomMenuItem::new("locale_cz".to_string(), "Czech");
+    let locale_en = CustomMenuItem::new("locale_en".to_string(), "English").selected();
+    let locale_fr = CustomMenuItem::new("locale_fr".to_string(), "French");
+    let locale_ge = CustomMenuItem::new("locale_ge".to_string(), "German");
+    let locale_hu = CustomMenuItem::new("locale_hu".to_string(), "Hungarian");
+    let locale_it = CustomMenuItem::new("locale_it".to_string(), "Italian");
+    let locale_jp = CustomMenuItem::new("locale_jp".to_string(), "Japanese");
+    let locale_kr = CustomMenuItem::new("locale_kr".to_string(), "Korean");
+    let locale_pl = CustomMenuItem::new("locale_pl".to_string(), "Polish");
+    let locale_po = CustomMenuItem::new("locale_po".to_string(), "Portugal");
+    let locale_sk = CustomMenuItem::new("locale_sk".to_string(), "Slovak");
+    let locale_es = CustomMenuItem::new("locale_es".to_string(), "Spanish");
+    let locale_es_mx = CustomMenuItem::new("locale_es_mx".to_string(), "Spanish Mexico");
+    let locale_tu = CustomMenuItem::new("locale_tu".to_string(), "Turkish");
+    let locale_ru = CustomMenuItem::new("locale_ru".to_string(), "Русский");
+    let file_submenu = Submenu::new("File", Menu::new().add_item(open).add_item(quit));
+    let locale_submenu = Submenu::new(
+        "Locale",
+        Menu::new()
+            .add_item(locale_cz)
+            .add_item(locale_en)
+            .add_item(locale_fr)
+            .add_item(locale_ge)
+            .add_item(locale_hu)
+            .add_item(locale_it)
+            .add_item(locale_jp)
+            .add_item(locale_kr)
+            .add_item(locale_pl)
+            .add_item(locale_po)
+            .add_item(locale_sk)
+            .add_item(locale_es)
+            .add_item(locale_es_mx)
+            .add_item(locale_tu)
+            .add_item(locale_ru),
+    );
 
     let menu = Menu::new()
-        // .add_native_item(MenuItem::Quit)
-        .add_submenu(submenu);
+        .add_submenu(file_submenu)
+        .add_submenu(locale_submenu);
 
     tauri::Builder::default()
             .plugin(tauri_plugin_log::Builder::default()
@@ -60,6 +95,7 @@ fn main() {
                 globals: None,
                 locale: None,
                 profile_file_path: None,
+                locale_lang: "locale_en".to_string(),
             })
 
         })
@@ -84,6 +120,38 @@ fn main() {
                             }
                     });
             }
+            "locale_cz" | "locale_en" | "locale_fr" | "locale_ge" | "locale_hu" | "locale_it" | "locale_jp" | "locale_kr" | "locale_pl" | "locale_po" | "locale_sk" | "locale_es" | "locale_es_mx" | "locale_tu" | "locale_ru" => {
+                let window = event.window();
+                let menu_handle = window.menu_handle();
+                let state: State<TarkovStashState>  = window.state();
+                let mut internal_state = state.state.lock().unwrap();
+                internal_state.locale_lang = event.menu_item_id().to_string();
+                let menu_item_id = event.menu_item_id().to_string();
+
+                if internal_state.profile_file_path.is_some() {
+                    window.emit("profile_loaded", "").expect("Can't emit event to window!");
+                }
+
+
+                std::thread::spawn(move || {
+                    menu_handle.get_item("locale_cz").set_selected(false).expect("Can't find menu item for locale_cz");
+                    menu_handle.get_item("locale_en").set_selected(false).expect("Can't find menu item for locale_en");
+                    menu_handle.get_item("locale_fr").set_selected(false).expect("Can't find menu item for locale_fr");
+                    menu_handle.get_item("locale_ge").set_selected(false).expect("Can't find menu item for locale_ge");
+                    menu_handle.get_item("locale_hu").set_selected(false).expect("Can't find menu item for locale_hu");
+                    menu_handle.get_item("locale_it").set_selected(false).expect("Can't find menu item for locale_it");
+                    menu_handle.get_item("locale_jp").set_selected(false).expect("Can't find menu item for locale_jp");
+                    menu_handle.get_item("locale_kr").set_selected(false).expect("Can't find menu item for locale_kr");
+                    menu_handle.get_item("locale_pl").set_selected(false).expect("Can't find menu item for locale_pl");
+                    menu_handle.get_item("locale_po").set_selected(false).expect("Can't find menu item for locale_po");
+                    menu_handle.get_item("locale_sk").set_selected(false).expect("Can't find menu item for locale_sk");
+                    menu_handle.get_item("locale_es").set_selected(false).expect("Can't find menu item for locale_es");
+                    menu_handle.get_item("locale_es_mx").set_selected(false).expect("Can't find menu item for locale_es_mx");
+                    menu_handle.get_item("locale_tu").set_selected(false).expect("Can't find menu item for locale_tu");
+                    menu_handle.get_item("locale_ru").set_selected(false).expect("Can't find menu item for locale_ru");
+                    menu_handle.get_item(&menu_item_id).set_selected(true).expect("Can't find selected menu item");
+                });
+            }
             _ => {}
         })
         .invoke_handler(tauri::generate_handler![load_profile_file, change_amount, change_fir, restore_durability, add_item, remove_item])
@@ -102,7 +170,7 @@ fn load_profile_file(state: State<TarkovStashState>) -> Result<UIProfile, String
             if verify_spt_folder(profile_file_path) {
                 create_backup(profile_file_path);
                 // save to state internal data
-                let locale = load_locale(profile_file_path);
+                let locale = load_locale(profile_file_path, internal_state.locale_lang.clone());
                 let bsg_items = load_bsg_items(profile_file_path);
                 let globals = load_globals(profile_file_path);
                 let bsg_items_root: HashMap<String, Value> =
@@ -297,7 +365,8 @@ fn load_globals(file: &String) -> String {
     fs::read_to_string(items).unwrap()
 }
 
-fn load_locale(file: &String) -> String {
+fn load_locale(file: &String, locale_menu_item: String) -> String {
+    let locale_id = format!("{}.json", locale_menu_item.replace("locale_", ""));
     let locale = Path::new(file)
         .ancestors()
         .nth(3)
@@ -307,9 +376,13 @@ fn load_locale(file: &String) -> String {
         .join("database")
         .join("locales")
         .join("global")
-        .join("en.json");
+        .join(locale_id.clone());
     locale.try_exists().expect(
-        "Can't find `en.json` in your `SPT\\Aki_Data\\Server\\database\\locales\\global` folder",
+        format!(
+            "Can't find {} in your `SPT\\Aki_Data\\Server\\database\\locales\\global` folder",
+            locale_id
+        )
+        .as_str(),
     );
     info!("Reading locale from {}", locale.display());
     fs::read_to_string(locale).unwrap()
