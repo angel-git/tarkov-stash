28b53c9bf62912d3d77eb11110e9fbcb01ae0770
agavalda <agavalda@digital.ai>
2023-11-15 19:21:00 +0100

    #82 add categories on list

--- src/routes/stash/modal/modal-item.svelte
+++ src/routes/stash/modal/modal-item.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   interface BsgItemWithParent extends BsgItem {
-    parent: string;
+    category: string;
+    name: string;
   }
 
   import { goto } from '$app/navigation';
@@ -16,20 +17,17 @@
   export let locale: Record<string, string>;
   export let grid: Array<Array<Item | undefined>>;
 
-  type OrderType = 'alpha' | 'parent';
-
   let showModal = true;
   let parsedItems: Array<BsgItemWithParent>;
   let parsedNodes: Record<string, BsgItem>;
-  const categories: Set<string> = new Set<string>();
+  let categories: Array<string>;
   let notEnoughSpaceError = false;
-  let order: OrderType = 'alpha';
 
   const sortByName = (a: BsgItemWithParent, b: BsgItemWithParent) => {
-    if (a.parent < b.parent) {
+    if (a.name < b.name) {
       return -1;
     }
-    if (a.parent > b.parent) {
+    if (a.name > b.name) {
       return 1;
     }
     return 0;
@@ -38,6 +36,8 @@
   $: if (!showModal) onClose();
 
   $: {
+    const categoriesSet: Set<string> = new Set<string>();
+
     parsedNodes = Object.keys(allItems)
       .map((i) => allItems[i])
       .filter((i) => i.type === 'Node')
@@ -54,10 +54,13 @@
         .filter((i) => getName(i.id, locale))
         .filter((i) => !getName(i.id, locale).includes('!!!DO_NOT_USE!!'))
         .forEach((i) => {
-          categories.add(getParentNode(i));
+          categoriesSet.add(getParentNode(i));
         });
 
-      console.log('categories', categories);
+      // remove wrong categories
+      categories = Array.from(categoriesSet)
+        .filter((c) => c !== 'inventory')
+        .sort();
 
       parsedItems = Object.keys(allItems)
         .map((i) => allItems[i])
@@ -65,22 +68,16 @@
         .filter((i) => !i.unbuyable)
         .filter((i) => getName(i.id, locale))
         .filter((i) => !getName(i.id, locale).includes('!!!DO_NOT_USE!!'))
-        .map((i) => {
-          if (order === 'alpha') {
-            return { ...i, parent: `${getName(i.id, locale)} - [${getParentNode(i)}]` };
-          } else {
-            return { ...i, parent: `[${getParentNode(i)}] - ${getName(i.id, locale)}` };
-          }
-        })
+        .map((i) => ({ ...i, category: getParentNode(i), name: getName(i.id, locale) }))
         .filter(
           (i) =>
-            i.parent.toLowerCase().includes($addNewItem.input.toLowerCase()) ||
-            getShortName(i.id, locale).toLowerCase().includes($addNewItem.input.toLowerCase()) ||
+            i.category.toLowerCase().includes($addNewItem.input.toLowerCase()) ||
+            i.name.toLowerCase().includes($addNewItem.input.toLowerCase()) ||
             $addNewItem.item?.id === i.id,
         )
         .sort(sortByName);
 
-      // preselet first item
+      // preselect first item
       if (!$addNewItem.item) {
         selectItem(parsedItems[0]);
       }
@@ -146,11 +143,11 @@
   }
 
   function getParentNode(item: BsgItem) {
-    return getName(parsedNodes[item.parentId]?.id, locale) || '??';
-  }
-
-  function onOrderChange(event: { currentTarget: HTMLInputElement }) {
-    order = event.currentTarget.value as OrderType;
+    return (
+      getName(parsedNodes[item.parentId]?.id, locale) ||
+      getName(parsedNodes[item.parentId]?.parentId, locale) ||
+      '??'
+    );
   }
 </script>
 
@@ -161,39 +158,28 @@
   <h2 slot="header">Add item into stash <strong>(BETA!)</strong></h2>
 
   <div class="modal-content">
-    <fieldset>
-      <input
-        type="radio"
-        id="alpha"
-        name="alpha"
-        value="alpha"
-        checked={order === 'alpha'}
-        on:change={onOrderChange}
-      />
-      <label for="alpha">Alpha ordering</label>
-      <input
-        type="radio"
-        id="parent"
-        name="parent"
-        value="parent"
-        checked={order === 'parent'}
-        on:change={onOrderChange}
-      />
-      <label for="parent">Parent ordering</label>
-    </fieldset>
     <!-- svelte-ignore a11y-autofocus -->
     <input autofocus placeholder="Filter..." bind:value={$addNewItem.input} />
     <div class="main">
-      <ul>
-        {#each parsedItems as item}
-          <li class={item.id === $addNewItem.item?.id ? 'selected' : ''}>
-            <button on:click={() => selectItem(item)}>{item.parent}</button>
-          </li>
+      <div class="left">
+        {#each categories as cat}
+          <section>
+            <h2>{cat}</h2>
+            <ul>
+              {#each parsedItems as item}
+                {#if item.category === cat}
+                  <li class={item.id === $addNewItem.item?.id ? 'selected' : ''}>
+                    <button on:click={() => selectItem(item)}>{item.name}</button>
+                  </li>
+                {/if}
+              {/each}
+            </ul>
+          </section>
         {/each}
-      </ul>
+      </div>
       {#if $addNewItem.item}
         <div
-          class="selected-item"
+          class="right"
           style={`background-color: ${calculateBackgroundColor($addNewItem.item.backgroundColor)}`}
         >
           <div>{getShortName($addNewItem.item.id, locale)}</div>
@@ -236,17 +222,27 @@
     overflow-y: auto;
   }
 
-  .main ul {
+  .main section {
+    display: flex;
+    flex-direction: column;
+  }
+  .main section h2 {
+    border-bottom: 1px solid var(--color-text);
+    font-size: 1.2em;
+    text-align: left;
+  }
+
+  .main .left {
     overflow-y: auto;
     flex: 1 0 50vh;
   }
 
-  .main .selected-item {
+  .main .right {
     flex: 1 1 50vh;
     padding: 8px;
   }
 
-  .main .selected-item .details {
+  .main .right .details {
     text-align: justify;
   }
 
@@ -268,13 +264,10 @@
 
   li button {
     border: none;
+    text-align: left;
   }
 
   li button:hover {
     color: var(--color-highlight);
-  }
-
-  fieldset {
-    border: none;
   }
 </style>
