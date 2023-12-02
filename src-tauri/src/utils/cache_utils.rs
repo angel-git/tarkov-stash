2ba9be3e19d4e144ff3dae016387d8402f7b1342
agavalda <agavalda@digital.ai>
2023-12-02 18:28:14 +0100

    #78 Add more utils

--- src-tauri/src/utils/cache_utils.rs
+++ src-tauri/src/utils/cache_utils.rs
@@ -1,3 +1,6 @@
+use serde_json::Value;
+use std::collections::HashMap;
+
 fn get_max_visible_ammo(bullets: u16, visible_ammo_ranges_string: &str) -> u16 {
     let visible_ammo_ranges = get_max_visible_ammo_ranges(visible_ammo_ranges_string);
 
@@ -55,10 +58,65 @@ fn get_deterministic_hash_code(s: &str) -> i32 {
     hash1.wrapping_add(hash2.wrapping_mul(1566083941))
 }
 
+fn is_ammo_item(tpl: &str, bsg_items_root: &HashMap<String, Value>) -> bool {
+    find_parent_by_name(bsg_items_root, tpl, "Ammo").is_some()
+}
+
+fn is_weapon_item(tpl: &str, bsg_items_root: &HashMap<String, Value>) -> bool {
+    find_parent_by_name(bsg_items_root, tpl, "Weapon").is_some()
+}
+
+fn is_magazine_item(tpl: &str, bsg_items_root: &HashMap<String, Value>) -> bool {
+    find_parent_by_name(bsg_items_root, tpl, "Magazine").is_some()
+}
+
+fn is_compound_without_hide_entrails(tpl: &str, bsg_items_root: &HashMap<String, Value>) -> bool {
+    if let Some(node) = bsg_items_root.get(tpl) {
+        if let Some(hide) = node
+            .get("_props")
+            .and_then(|p| p.get("HideEntrails").and_then(|hide| hide.as_bool()))
+        {
+            if !hide {
+                if let Some(slots) = node.get("_props").and_then(|p| p.get("Slots")) {
+                    slots.as_array().is_some_and(|slot| !slot.is_empty())
+                } else {
+                    false
+                }
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    } else {
+        false
+    }
+}
+
+fn find_parent_by_name(
+    bsg_items_root: &HashMap<String, Value>,
+    current_id: &str,
+    target_name: &str,
+) -> Option<Value> {
+    if let Some(node) = bsg_items_root.get(current_id) {
+        let name = node.get("_name").unwrap().as_str().unwrap();
+        if name == target_name {
+            Some(node.clone())
+        } else if let Some(parent_id) = node.get("_parent") {
+            find_parent_by_name(bsg_items_root, &parent_id.as_str().unwrap(), target_name)
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::utils::cache_utils::{
         get_deterministic_hash_code, get_max_visible_ammo, get_max_visible_ammo_ranges,
+        is_ammo_item, is_compound_without_hide_entrails,
     };
     use serde_json::Value;
     use std::collections::HashMap;
@@ -159,5 +217,45 @@ mod tests {
             get_deterministic_hash_code("f5b4f4741134ec027112e83b"),
             1795904484
         );
+    }
+
+    #[test]
+    fn should_item_be_ammo() {
+        let bsg_items_root: HashMap<String, Value> = serde_json::from_str(
+            String::from_utf8_lossy(include_bytes!(
+                "../../../example/Aki_Data/Server/database/templates/items.json"
+            ))
+            .as_ref(),
+        )
+        .unwrap();
+        let is_ammo = is_ammo_item("5ea2a8e200685063ec28c05a", &bsg_items_root);
+        assert!(is_ammo);
+    }
+
+    #[test]
+    fn should_container_not_be_compound_without_hide_entrails() {
+        let bsg_items_root: HashMap<String, Value> = serde_json::from_str(
+            String::from_utf8_lossy(include_bytes!(
+                "../../../example/Aki_Data/Server/database/templates/items.json"
+            ))
+            .as_ref(),
+        )
+        .unwrap();
+        let backpack =
+            is_compound_without_hide_entrails("5e9dcf5986f7746c417435b3", &bsg_items_root);
+        assert!(!backpack);
+    }
+
+    #[test]
+    fn should_weapon_be_compound_without_hide_entrails() {
+        let bsg_items_root: HashMap<String, Value> = serde_json::from_str(
+            String::from_utf8_lossy(include_bytes!(
+                "../../../example/Aki_Data/Server/database/templates/items.json"
+            ))
+            .as_ref(),
+        )
+        .unwrap();
+        let weapon = is_compound_without_hide_entrails("59e6152586f77473dc057aa1", &bsg_items_root);
+        assert!(weapon);
     }
 }
