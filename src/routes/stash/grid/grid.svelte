cfc51f22724ea5b04bc472e1177c6905bac6f27e
agavalda <agavalda@digital.ai>
2024-02-04 23:35:43 +0100

    #99 Update context menu css

--- src/routes/stash/grid/grid.svelte
+++ src/routes/stash/grid/grid.svelte
@@ -292,10 +292,10 @@
 
   .options .option {
     font-size: 11px;
-    padding: 4px 10px;
+    padding: 2px 10px;
     margin: 2px 0;
-    border-top-left-radius: 10px;
-    border-bottom-right-radius: 10px;
+    border-top-left-radius: 6px;
+    border-bottom-right-radius: 6px;
     text-transform: uppercase;
     background-color: var(--color-menu);
   }
