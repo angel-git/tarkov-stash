2aae0ae1bdd1b0b8d2bd2e0522758e329098fc92
agavalda <agavalda@digital.ai>
2024-04-06 01:04:53 +0200

    #113 Clippy!

--- src-tauri/src/app/handles.rs
+++ src-tauri/src/app/handles.rs
@@ -65,7 +65,8 @@ pub async fn load_profile_file(
                                 drop(internal_state);
                                 ui_profile.spt_version =
                                     Some(get_server_version(profile_file_path));
-                                if ui_profile.unknown_items.len() > 0 {
+                                // let's disable add items feature if custom items are present
+                                if !ui_profile.unknown_items.is_empty() {
                                     update_enable_add_items(&window, Some(false));
                                 }
                                 Ok(ui_profile)
