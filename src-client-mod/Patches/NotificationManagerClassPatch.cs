b872082e25d77502d945f2e9c394ae56b9f871dc
agavalda <agavalda@digital.ai>
2024-05-19 12:27:55 +0200

    #151 Add client mod

---
+++ src-client-mod/Patches/NotificationManagerClassPatch.cs
@@ -0,0 +1,25 @@
+﻿using Aki.Reflection.Patching;
+using HarmonyLib;
+using System.Reflection;
+
+namespace TarkovStash.Patches
+{
+
+    internal class NotificationManagerClassPatch : ModulePatch
+    {
+
+
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(NotificationManagerClass), nameof(NotificationManagerClass.method_7));
+        }
+
+        [PatchPostfix]
+        public static void PatchPostfix(string message)
+        {
+            Plugin.Instance.handleMessage(message);
+        }
+
+    }
+
+}
